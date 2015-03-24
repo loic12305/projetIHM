d5ddbce7b81b213ab4c57abfc0d8d2dc672ea853
lentressangle <lentressangle@naoned-systemes.fr>
2015-03-24 17:00:58 +0100

    remove Acme :: add FOSUserBundler work :: clean twig

---
+++ src/projetIHM/UserBundle/Entity/User.php
@@ -0,0 +1,28 @@
+<?php
+// src/Acme/UserBundle/Entity/User.php
+
+namespace projetIHM\UserBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+  /**
+   * @ORM\Column(name="id", type="integer")
+   * @ORM\Id
+   * @ORM\GeneratedValue(strategy="AUTO")
+   */
+  protected $id;
+
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
---
+++ src/projetIHM/UserBundle/projetIHMUserBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace projetIHM\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class projetIHMUserBundle extends Bundle
+{
+	public function getParent()
+	{
+		return 'FOSUserBundle';
+	}
+
+}
